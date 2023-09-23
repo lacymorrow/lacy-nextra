ba3585c7591cd0271fced0273a7fc94c27fb89bc
Lacy Morrow <me@lacymorrow.com>
2023-09-22 22:40:16 -0400

    layout

---
+++ src/components/flash/ruffle.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "@/vendor/ruffle/ruffle.js";
+type Props = {};
+
+export const Ruffle = (props: Props) => {
+  return <div>Ruffle</div>;
+};
+
+export default Ruffle;
